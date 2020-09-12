88f13439f28a3138bd9ed1b5045e9271066ce77e
Shivam <shivam.kumar@go-jek.com>
2020-09-13 02:32:35 +0530

    fixed issue of IG search

--- ig/ig.go
+++ ig/ig.go
@@ -180,6 +180,13 @@ func GetFollowers(userName string, MaxFollowers string) []string {
 		MaxFollowersInt = 500
 	}
 	MaxFollowersCount := 0
+	UserID := GetUserID(userName)
+	fmt.Println("*****")
+	fmt.Println("User Id found: " + UserID)
+	fmt.Println("*****")
+	if UserID == "" {
+		return nil
+	}
 	var finalValues []string
 	var igFollowersResearch IGFollowersResearch
 	EndCursor := "first"
@@ -187,9 +194,9 @@ func GetFollowers(userName string, MaxFollowers string) []string {
 	for NextPage && MaxFollowersCount < MaxFollowersInt {
 		var URL string
 		if EndCursor == "first" {
-			URL = "https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=%7B%22id%22%3A%22" + GetUserID(userName) + "%22%2C%22include_reel%22%3Afalse%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A24%7D"
+			URL = "https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=%7B%22id%22%3A%22" + UserID + "%22%2C%22include_reel%22%3Afalse%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A24%7D"
 		} else {
-			URL = "https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=%7B%22id%22%3A%22" + GetUserID(userName) + "%22%2C%22include_reel%22%3Afalse%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A12%2C%22after%22%3A%22" + EndCursor[:len(EndCursor)-2] + "%3D%3D%22%7D"
+			URL = "https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=%7B%22id%22%3A%22" + UserID + "%22%2C%22include_reel%22%3Afalse%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A12%2C%22after%22%3A%22" + EndCursor[:len(EndCursor)-2] + "%3D%3D%22%7D"
 		}
 		fmt.Println(URL)
 		// resp, err := soup.Get(URL)
